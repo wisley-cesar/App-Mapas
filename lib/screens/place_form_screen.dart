9b367e27873d6fe0349de352e258e14372337894
Wisley César <wisleycesar32@gmail.com>
2025-01-28 10:35:38 -0300

    FEAT: Criando o widget que vai mostrar o mapa

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:app_mapas/providers/great_places.dart';
 import 'package:app_mapas/widgets/image_input.dart';
+import 'package:app_mapas/widgets/location_input.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -62,6 +63,8 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl == null
+              ? Text('Localização não Informada')
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              icon: Icon(
+                Icons.location_on,
+                size: 20,
+              ),
+              label: Text(
+                'Localização Atual',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              onPressed: () {},
+            ),
+            TextButton.icon(
+              icon: Icon(
+                Icons.map,
+                size: 20,
+              ),
+              label: Text(
+                'Selecione no Mapa',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
