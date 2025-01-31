fc7e094499ef9251a4ba5138dc225b3ccc7dae31
Wisley César <wisleycesar32@gmail.com>
2025-01-31 17:01:26 -0300

    FEAT: Finalizando o projeto

--- lib/utils/location_util.dart
+++ lib/utils/location_util.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
-
+const googleApiKey = 'AIzaSyADhYfYiwOvYdoTjRglsCQ5MSKW2jSLl0c';
 
 class LocationUtil {
   static String generateLocationPreviewImage({
